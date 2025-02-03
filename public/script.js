671ce763a367ae9907ee3adb461840f2d9e6281e
Amir H. Mohammadi <25364126+amirify@users.noreply.github.com>
2025-02-04 01:03:50 +0330

    add weather summary functionality

--- public/script.js
+++ public/script.js
@@ -1,13 +1,25 @@
 document.addEventListener("DOMContentLoaded", () => {
   const openBtn = document.getElementById("open-app-btn");
   const closeBtn = document.getElementById("close-app-btn");
+
+  // This element wraps the weather-app to control
+  // the visibility of weather-app styles when
+  // it is unmounted
   const weatherAppContainer = document.getElementById("weather-app-container");
 
+  // Remove old weather data after 24 Hour
+  removeOldWeatherSummary();
+
   openBtn.addEventListener("click", () => {
+    // Hide the Open Button when weather-app is already open
     openBtn.classList.add("app-hidden");
+
+    // Make the weather-app container visible
     weatherAppContainer.classList.remove("app-hidden");
 
-    // Fetch the manifest file
+    // Fetch the manifest file to get the latest
+    // module and styles file names on weather-app host
+    // and then load them dynamically inside the landing page
     fetch("https://weather-app-d2a9a.web.app/.vite/manifest.json")
       .then((response) => response.json())
       .then((manifest) => {
@@ -24,6 +36,9 @@ document.addEventListener("DOMContentLoaded", () => {
         }
 
         // Dynamically load the weather app script if it's not already loaded
+        // mountWeatherApp and unmountWeatherApp methods are exposed by weather app
+        // so we can unmount it upon app close and remount it
+        // with fresh data when we open the app again
         if (window.mountWeatherApp) {
           window.mountWeatherApp();
         } else {
@@ -37,10 +52,44 @@ document.addEventListener("DOMContentLoaded", () => {
   });
 
   closeBtn.addEventListener("click", () => {
+    // unmount the weather-app
     if (window.unmountWeatherApp) {
+      // Show the Open Button when weather-app is closed
       openBtn.classList.remove("app-hidden");
+
+      // hide weather-app container when it's closed
       weatherAppContainer.classList.add("app-hidden");
       window.unmountWeatherApp();
+
+      // display weather summary info stored by 
+      // weather-app in localStorage
+      displayWeatherSummary();
     }
   });
+
+  // Remove old weather data after 24 Hour
+  function removeOldWeatherSummary() {
+    const storedData = JSON.parse(localStorage.getItem("weatherSummary"));
+    if (
+      storedData &&
+      new Date() - new Date(storedData.lastUpdated) > 86400000
+    ) {
+      localStorage.removeItem("weatherSummary");
+    }
+  }
+
+  // Read weather summary from localStorage
+  // and display it inside landing page
+  // we call this function when weather app is closed
+  function displayWeatherSummary() {
+    const weatherSummary = localStorage.getItem("weatherSummary");
+    if (weatherSummary) {
+      const { name, date, main, description } = JSON.parse(weatherSummary);
+      document.getElementById("weather-summary").innerHTML = `
+        <h2>Weather in ${name}</h2>
+        <p>${description} | Temperature: ${main.temp}°C</p>
+        <small>Last Updated: ${date}</small>
+      `;
+    }
+  }
 });
