1f26fa38c5179f3662c3f609887122e2bcb48970
Amir H. Mohammadi <25364126+amirify@users.noreply.github.com>
2025-02-03 22:17:14 +0330

    add dynamic module resolution through fetching manifest.json

--- public/script.js
+++ public/script.js
@@ -2,31 +2,38 @@ document.addEventListener("DOMContentLoaded", () => {
   const openBtn = document.getElementById("open-app-btn");
   const closeBtn = document.getElementById("close-app-btn");
   const weatherAppContainer = document.getElementById("weather-app-container");
-  const stylesFileName = "index-C_pK16rU.css";
-  const moduleFileName = "index-Df8klYYZ.js";
 
   openBtn.addEventListener("click", () => {
     openBtn.classList.add("app-hidden");
     weatherAppContainer.classList.remove("app-hidden");
 
-    // Dynamically load the weather app styles
-    if (!document.getElementById("weather-app-styles")) {
-      const styleLink = document.createElement("link");
-      styleLink.id = "weather-app-styles";
-      styleLink.rel = "stylesheet";
-      styleLink.href = `https://weather-app-d2a9a.web.app/assets/${stylesFileName}`;
-      document.head.appendChild(styleLink);
-    }
+    // Fetch the manifest file
+    fetch("https://weather-app-d2a9a.web.app/.vite/manifest.json")
+      .then((response) => response.json())
+      .then((manifest) => {
+        const moduleFile = manifest["index.html"].file;
+        const cssFile = manifest["index.html"].css?.[0];
 
-    // Dynamically load the weather app script if it's not already loaded
-    if (window.mountWeatherApp) {
-      window.mountWeatherApp();
-    } else {
-      const script = document.createElement("script");
-      script.src = `https://weather-app-d2a9a.web.app/assets/${moduleFileName}`;
-      script.type = "module";
-      document.body.appendChild(script);
-    }
+        // Dynamically load the weather app styles
+        if (!document.getElementById("weather-app-styles")) {
+          const styleLink = document.createElement("link");
+          styleLink.id = "weather-app-styles";
+          styleLink.rel = "stylesheet";
+          styleLink.href = `https://weather-app-d2a9a.web.app/${cssFile}`;
+          document.head.appendChild(styleLink);
+        }
+
+        // Dynamically load the weather app script if it's not already loaded
+        if (window.mountWeatherApp) {
+          window.mountWeatherApp();
+        } else {
+          const script = document.createElement("script");
+          script.src = `https://weather-app-d2a9a.web.app/${moduleFile}`;
+          script.type = "module";
+          document.body.appendChild(script);
+        }
+      })
+      .catch((err) => console.error("Failed to load manifest:", err));
   });
 
   closeBtn.addEventListener("click", () => {
